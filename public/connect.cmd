9ce08b95061626aeb3967405e6854b4d21a6641c
Daniel Li <inflmts@gmail.com>
2024-12-02 18:21:38 -0500

    Remove client files

--- public/connect.cmd
+++ public/connect.cmd
@@ -1,1 +1,1 @@
-@ssh -q -L 25452:localhost:25452 -o ExitOnForwardFailure=yes -o BatchMode=yes -i %USERPROFILE%\.ssh\id_cisecraft li.daniel@storm.cise.ufl.edu
+@ssh -q -L 25452:localhost:25452 -o ExitOnForwardFailure=yes li.daniel@storm.cise.ufl.edu "exec ~/cisecraft/connect-shell"
